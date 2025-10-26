d4f1a381333935f8f0cea1ff26b73a5172e34e9b
Absar Ahmad <absarahmad137@gmail.com>
2025-10-26 14:34:21 +0530

    added User model and updated server.js

--- halal-checker-backend/server.js
+++ halal-checker-backend/server.js
@@ -2,20 +2,25 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/database');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const flash = require('express-flash')
 const ingredients = require('./routes/ingredientsRoutes');
 require('dotenv').config({ path: './config/.env' })
 
-const PORT = process.env.PORT || 8001
+
 
 
 connectDB()
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
+app.use(flash());
 
 // Routes
 app.use('/api/ingredients', ingredients);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running you better catch it`)
 })
